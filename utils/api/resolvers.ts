226b93b8dfad8296970c59ba2451cf2224f19405
Stephen Jensen <stephen.jensen3@gmail.com>
2020-12-21 18:22:48 -0600

    7. Add Bundle queries and mutations

--- utils/api/resolvers.ts
+++ utils/api/resolvers.ts
@@ -1,13 +1,21 @@
 export const resolvers = {
- Query: {
-     hello: (parent, args, context) => 'hi!',
-     feed: (parent, {data: { id }}, {prisma}) => prisma.feed.findUnique({where: {id}}),
-     feeds: (parent, args, {prisma}) => prisma.feed.findMany()
- },
- Mutation: {
-     createFeed: async (parent, {data}, {prisma, user}) => {
-         const result = await prisma.feed.create({data: {...data}})
-         return result
-     }
- }   
-}
+  Query: {
+    hello: (parent, args, context) => 'hi!',
+    feed: (parent, { data: { id } }, { prisma }) =>
+      prisma.feed.findUnique({ where: { id } }),
+    feeds: (parent, args, { prisma }) => prisma.feed.findMany(),
+    bundle: (parent, { data: { id } }, { prisma }) =>
+      prisma.bundle.findUnique({ where: { id } }),
+    bundles: (parent, args, { prisma }) => prisma.bundle.findMany(),
+  },
+  Mutation: {
+    createFeed: async (parent, { data }, { prisma, user }) => {
+      const result = await prisma.feed.create({ data: { ...data } });
+      return result;
+    },
+    createBundle: async (parent, { data }, { prisma, user }) => {
+      const result = await prisma.bundle.create({ data: { ...data } });
+      return result;
+    },
+  },
+};
--- utils/api/typeDefs.ts
+++ utils/api/typeDefs.ts
@@ -1,27 +1,43 @@
 import { gql } from 'apollo-server-micro';
 
 export const typeDefs = gql`
-    type Feed {
-        id: String
-        name: String
-        url: String
-    }
-    input FeedInput {
-        id: String
-    }
+  type Feed {
+    id: String
+    name: String
+    url: String
+  }
+  type Bundle {
+    id: String
+    name: String
+    description: String
+  }
+  input FeedInput {
+    id: String
+  }
+  input BundleInput {
+    id: String
+  }
 
-    input FeedCreateInput {
-        id: String
-        url: String
-        name: String
-    }
+  input FeedCreateInput {
+    id: String
+    url: String
+    name: String
+  }
+  input BundleCreateInput {
+    id: String
+    name: String
+    description: String
+  }
 
-    type Query {
-        hello: String
-        feed(data: FeedInput): Feed
-        feeds: [Feed]
-    }
-    type Mutation {
-        createFeed(data: FeedCreateInput): Feed
-    }
-`
+  type Query {
+    hello: String
+    feed(data: FeedInput): Feed
+    bundle(data: BundleInput): Bundle
+    feeds: [Feed]
+    bundles: [Bundle]
+  }
+  type Mutation {
+    createFeed(data: FeedCreateInput): Feed
+    createBundle(data: BundleCreateInput): Bundle
+  }
+`;
