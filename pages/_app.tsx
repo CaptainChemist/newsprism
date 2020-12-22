161382193f17076b5ba4e1c6e7242354b4af064e
Stephen Jensen <stephen.jensen3@gmail.com>
2020-12-22 16:16:30 -0600

    16. Add Tailwindcss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import { ApolloProvider } from '@apollo/client';
 import { useApollo } from '../utils/apolloClient';
+import 'tailwindcss/tailwind.css';
+import '../styles/index.css';
 
 export default function App({ Component, pageProps }) {
   const apolloClient = useApollo(pageProps.initialApolloState);
