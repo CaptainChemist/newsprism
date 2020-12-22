675f898bf721e42f68e19c23eed39f15d40ceafe
Stephen Jensen <stephen.jensen3@gmail.com>
2020-12-22 16:03:04 -0600

    3. Configure Auth0
    formatting
    
    
    add app.tsx
    
    
    changed cookie var

---
+++ next.config.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const {
+  DATABASE_URL,
+  AUTH0_CLIENTID,
+  AUTH0_DOMAIN,
+  AUTH0_CLIENT_SECRET,
+  AUTH0_SCOPE,
+  AUTH0_COOKIE,
+  BACKEND_ADDRESS,
+} = process.env;
+
+module.exports = {
+  publicRuntimeConfig: {
+    BACKEND_URL: `${BACKEND_ADDRESS}/api/graphql`,
+  },
+  serverRuntimeConfig: {
+    auth: {
+      domain: AUTH0_DOMAIN,
+      clientId: AUTH0_CLIENTID,
+      clientSecret: AUTH0_CLIENT_SECRET,
+      scope: AUTH0_SCOPE,
+      redirectUri: `${BACKEND_ADDRESS}/api/callback`,
+      postLogoutRedirectUri: `${BACKEND_ADDRESS}/`,
+    },
+    cookieSecret: AUTH0_COOKIE,
+  },
+};
---
+++ utils/apolloClient.ts
@@ -0,0 +1,43 @@
+import { useMemo } from 'react';
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import getConfig from 'next/config';
+
+const { publicRuntimeConfig } = getConfig();
+const { BACKEND_URL } = publicRuntimeConfig;
+
+let apolloClient;
+
+function createApolloClient() {
+  return new ApolloClient({
+    ssrMode: typeof window === 'undefined',
+    link: new HttpLink({
+      uri: BACKEND_URL, // Server URL (must be absolute)
+      credentials: 'same-origin', // Additional fetch() options like `credentials` or `headers`
+    }),
+    cache: new InMemoryCache({}),
+  });
+}
+
+export function initializeApollo(initialState = null) {
+  const _apolloClient = apolloClient ?? createApolloClient();
+
+  // If your page has Next.js data fetching methods that use Apollo Client, the initial state
+  // gets hydrated here
+  if (initialState) {
+    // Get existing cache, loaded during client side data fetching
+    const existingCache = _apolloClient.extract();
+
+    _apolloClient.cache.restore({ ...existingCache, ...initialState });
+  }
+  // For SSG and SSR always create a new Apollo Client
+  if (typeof window === 'undefined') return _apolloClient;
+  // Create the Apollo Client once in the client
+  if (!apolloClient) apolloClient = _apolloClient;
+
+  return _apolloClient;
+}
+
+export function useApollo(initialState) {
+  const store = useMemo(() => initializeApollo(initialState), [initialState]);
+  return store;
+}
